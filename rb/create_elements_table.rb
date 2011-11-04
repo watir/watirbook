2530cde84e0f6c020ebc8c16bb972b001cd0afd8
Zeljko Filipin <zeljko.filipin@gmail.com>
2011-11-04 23:04:17 +0100

    better table for supported elements

--- rb/create_elements_table.rb
+++ rb/create_elements_table.rb
@@ -1,5 +1,125 @@
-def elements_and_collections
-  [[:a, :as], [:abbr, :abbrs], [:address, :addresses], [:area, :areas], [:article, :articles], [:aside, :asides], [:audio, :audios], [:b, :bs], [:base, :bases], [:bdi, :bdis], [:bdo, :bdos], [:blockquote, :blockquotes], [:body, :bodys], [:br, :brs], [:button, :buttons], [:canvas, :canvases], [:caption, :captions], [:checkbox, :checkboxes], [:cite, :cites], [:code, :codes], [:col, :cols], [:colgroup, :colgroups], [:command, :commands], [:datalist, :datalists], [:dd, :dds], [:del, :dels], [:details, nil], [:dfn, :dfns], [:div, :divs], [:dl, :dls], [:dt, :dts], [:element, :elements], [:element_by_xpath, :elements_by_xpath], [:em, :ems], [:embed, :embeds], [:fieldset, :fieldsets], [:figcaption, :figcaptions], [:figure, :figures], [:file_field, :file_fields], [:font, :fonts], [:footer, :footers], [:form, :forms], [:frame, :frames], [:h1, :h1s], [:h2, :h2s], [:h3, :h3s], [:h4, :h4s], [:h5, :h5s], [:h6, :h6s], [:head, :heads], [:header, :headers], [:hgroup, :hgroups], [:hidden, :hiddens], [:hr, :hrs], [:html, :htmls], [:i, :is], [:iframe, :iframes], [:image, :images], [:img, :imgs], [:input, :inputs], [:ins, :inses], [:kbd, :kbds], [:keygen, :keygens], [:label, :labels], [:legend, :legends], [:li, :lis], [:link, :links], [:map, :maps], [:mark, :marks], [:menu, :menus], [:meta, :metas], [:meter, :meters], [:nav, :navs], [:noscript, :noscripts], [:object, :objects], [:ol, :ols], [:optgroup, :optgroups], [:option, :options], [:output, :outputs], [:p, :ps], [:param, :params], [:pre, :pres], [:progress, :progresses], [:q, :qs], [:radio, :radios], [:rp, :rps], [:rt, :rts], [:ruby, :rubies], [:s, :ss], [:samp, :samps], [:script, :scripts], [:section, :sections], [:select, :selects], [:select_list, :select_lists], [:small, :smalls], [:source, :sources], [:span, :spans], [:strong, :strongs], [:style, :styles], [:sub, :subs], [:summary, :summaries], [:sup, :sups], [:table, :tables], [:tbody, :tbodys], [:td, :tds], [:text_field, :text_fields], [:textarea, :textareas], [:tfoot, :tfoots], [:th, :ths], [:thead, :theads], [:time, :times], [:title, :titles], [:tr, :trs], [:track, :tracks], [:u, :us], [:ul, :uls], [:var, :vars], [:video, :videos], [:wbr, :wbrs]]
+def data
+  [
+  [:a, :as, true, true, true],
+  [:abbr, :abbrs, true, false, false],
+  [:address, :addresses, true, false, false],
+  [:area, :areas, true, true, true],
+  [:article, :articles, true, false, false],
+  [:aside, :asides, true, false, false],
+  [:audio, :audios, true, false, false],
+  [:b, :bs, true, false, false],
+  [:base, :bases, true, false, false],
+  [:bdi, :bdis, true, false, false],
+  [:bdo, :bdos, true, false, false],
+  [:blockquote, :blockquotes, true, false, false],
+  [:body, :bodys, true, false, false],
+  [:br, :brs, true, false, false],
+  [:button, :buttons, true, true, true],
+  [:canvas, :canvases, true, false, false],
+  [:caption, :captions, true, false, false],
+  [:checkbox, :checkboxes, false, true, true],
+  [:cite, :cites, true, false, false],
+  [:code, :codes, true, false, false],
+  [:col, :cols, true, false, false],
+  [:colgroup, :colgroups, true, false, false],
+  [:command, :commands, true, false, false],
+  [:datalist, :datalists, true, false, false],
+  [:dd, :dds, true, true, true],
+  [:del, :dels, true, true, false],
+  [:details, nil, true, false, false],
+  [:dfn, :dfns, true, false, false],
+  [:div, :divs, true, true, true],
+  [:dl, :dls, true, true, true],
+  [:dt, :dts, true, true, true],
+  [:element, :elements, false, false, false],
+  [:element_by_xpath, :elements_by_xpath, false, false, false],
+  [:em, :ems, true, true, true],
+  [:embed, :embeds, true, false, false],
+  [:fieldset, :fieldsets, true, false, false],
+  [:figcaption, :figcaptions, true, false, false],
+  [:figure, :figures, true, false, false],
+  [:file_field, :file_fields, false, true, true],
+  [:font, :fonts, true, false, false],
+  [:footer, :footers, true, false, false],
+  [:form, :forms, true, true, true],
+  [:frame, :frames, true, true, true],
+  [:h1, :h1s, true, true, false],
+  [:h2, :h2s, true, true, false],
+  [:h3, :h3s, true, true, false],
+  [:h4, :h4s, true, true, false],
+  [:h5, :h5s, true, true, false],
+  [:h6, :h6s, true, true, false],
+  [:head, :heads, true, false, false],
+  [:header, :headers, true, false, true],
+  [:hgroup, :hgroups, true, false, false],
+  [:hidden, :hiddens, false, false, false],
+  [:hr, :hrs, true, false, false],
+  [:html, :htmls, true, false, false],
+  [:i, :is, true, false, false],
+  [:iframe, :iframes, true, true, true],
+  [:image, :images, false, true, true],
+  [:img, :imgs, true, true, true],
+  [:input, :inputs, true, true, true],
+  [:ins, :inses, true, true, false],
+  [:kbd, :kbds, true, false, false],
+  [:keygen, :keygens, true, false, false],
+  [:label, :labels, true, true, true],
+  [:legend, :legends, true, false, false],
+  [:li, :lis, true, true, true],
+  [:link, :links, false, true, true],
+  [:map, :maps, true, true, true],
+  [:mark, :marks, true, false, false],
+  [:menu, :menus, true, false, false],
+  [:meta, :metas, true, true, false],
+  [:meter, :meters, true, false, false],
+  [:nav, :navs, true, false, false],
+  [:noscript, :noscripts, true, false, false],
+  [:object, :objects, true, false, false],
+  [:ol, :ols, true, true, true],
+  [:optgroup, :optgroups, true, false, false],
+  [:option, :options, true, false, true],
+  [:output, :outputs, true, false, false],
+  [:p, :ps, true, true, true],
+  [:param, :params, true, false, false],
+  [:pre, :pres, true, true, true],
+  [:progress, :progresses, true, false, false],
+  [:q, :qs, true, false, false],
+  [:radio, :radios, false, true, true],
+  [:rp, :rps, true, false, false],
+  [:rt, :rts, true, false, false],
+  [:ruby, :rubies, true, false, false],
+  [:s, :ss, true, false, false],
+  [:samp, :samps, true, false, false],
+  [:script, :scripts, true, false, false],
+  [:section, :sections, true, false, false],
+  [:select, :selects, true, true, true],
+  [:select_list, :select_lists, false, true, true],
+  [:small, :smalls, true, false, false],
+  [:source, :sources, true, false, false],
+  [:span, :spans, true, true, true],
+  [:strong, :strongs, true, true, true],
+  [:style, :styles, true, false, false],
+  [:sub, :subs, true, false, false],
+  [:summary, :summaries, true, false, false],
+  [:sup, :sups, true, false, false],
+  [:table, :tables, true, true, true],
+  [:tbody, :tbodys, true, true, false],
+  [:td, :tds, true, true, true],
+  [:text_field, :text_fields, false, true, true],
+  [:textarea, :textareas, true, true, true],
+  [:tfoot, :tfoots, true, false, false],
+  [:th, :ths, true, false, false],
+  [:thead, :theads, true, false, false],
+  [:time, :times, true, false, false],
+  [:title, :titles, true, false, false],
+  [:tr, :trs, true, true, true],
+  [:track, :tracks, true, false, false],
+  [:u, :us, true, false, false],
+  [:ul, :uls, true, true, true],
+  [:var, :vars, true, false, false],
+  [:video, :videos, true, false, false],
+  [:wbr, :wbrs, true, false, false]
+  ]
 end
 def create_html(body)
   header = "<html><head><title>HTML elements</title><style type=\"text/css\">.green {background-color:#adff2f;} th {width:5em;}</style></head><body><table border=\"1\"><tbody><tr><th>element</th><th>collection</th><th>html</th><th>watir-webdriver</th><th>watir</th><th>safariwatir</th>"
@@ -10,10 +130,38 @@ end
 
 def body
   table = ""
-  elements_and_collections.each do |element_and_collection|
-    element = element_and_collection[0]
-    collection = element_and_collection[1]
-    table << "<tr><td>#{element}</td><td>#{collection}</td><td>&lt;#{element}&gt;</td><td class=\"green\"></td><td></td><td></td></tr>\n"
+  data.each do |data|
+    element = data[0]
+    collection = data[1]
+    has_default_html = data[2]
+    watir = data[3]
+    safariwatir = data[4]
+
+    empty_cell = "<td></td>"
+
+    default_html = "<td>&lt;#{element}&gt;</td>"
+    if has_default_html
+      html = default_html
+    else
+      html = empty_cell
+    end
+
+    green_cell = "<td class=\"green\">"
+    watir_webdriver_cell = green_cell
+
+    if watir
+      watir_cell = green_cell
+    else
+      watir_cell = empty_cell
+    end
+
+    if safariwatir
+      safariwatir_cell = green_cell
+    else
+      safariwatir_cell = empty_cell
+    end
+
+    table << "<tr><td>#{element}</td><td>#{collection}</td>#{html}#{watir_webdriver_cell}#{watir_cell}#{safariwatir_cell}</tr>\n"
   end
   table
 end
