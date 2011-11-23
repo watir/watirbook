d21743ec5c09dcc3ebea55c3ca1b71a359ee0d0b
Zeljko Filipin <zeljko.filipin@gmail.com>
2011-11-23 20:12:05 +0100

    create table for one element, works only for link at the moment

---
+++ rb/create_element_table.rb
@@ -0,0 +1,57 @@
+def html_class
+  html(%{ class="click-me"})
+end
+def html_double
+  %{#{html}<br>#{html}}
+end
+def html_id
+  html(%{ id="click-me"})
+end
+def html_name
+  html(%{ name="click-me"})
+end
+def html_title
+  html(%{ title="click-me"})
+end
+def regexp(how)
+  %{browser.a(#{how} => /click/).click}
+end
+def string(how)
+  %{browser.a(#{how} => "click-me").click}
+end
+
+def create_html(body)
+  header = "<html><head><title>a element</title></head><body><h1>a element</h1><table border=\"1\"><tbody><tr><th>how</th><th>html</th><th>regexp</th><th>string</th><th>other</th>"
+  footer = "</tbody></table></body></html>"
+  table = header + body + footer
+  File.open("../reference/a.htm", "w") {|file| file.write(table)}
+end
+def body
+  table = ""
+  a.each do |how, data|
+    if data
+
+      html = ""
+      regexp = ""
+      string = ""
+      other = ""
+      data.each do |key, value|
+        if key == :html
+          require "cgi"
+          html = CGI::escapeHTML(value).gsub("&lt;br&gt;", "<br>")
+        elsif key == :regexp
+          regexp = value
+        elsif key == :string
+          string = value
+        else
+          other << "#{key}: #{value}<br>"
+        end
+      end
+
+      table << "<tr><td>#{how}</td><td>#{html}</td><td>#{regexp}</td><td>#{string}</td><td>#{other}</td></tr>"
+    else
+      table << "<tr><td>#{how}</td><td colspan='4'>not supported</td></tr>"
+    end
+  end
+  table
+end
--- rb/link.rb
+++ rb/link.rb
@@ -1,80 +1,93 @@
-simple_html_example = %{<a href="clicked.htm">click me</a>}
+require "./create_element_table"
 
-{ action: false,
+def a
+  {
+  action: false,
 
   after: {
-    element: %{browser.a(:after? => browser.a(:id => "click-me")).flash},
-    html:    %{#{simple_html_example}\n<a href="clicked.htm" id="click-me">click me</a>
-    #{simple_html_example}}},
+    element: %{browser.a(:after? => #{string(:id)}).flash},
+    html:    %{#{html}<br>#{html_id}<br>#{html}}},
 
   alt: false,
 
   class: {
-    html:   %{<a href="clicked.htm" class="click-me">click me</a>},
-    regexp: %{browser.a(:class => /click/).click},
-    string: %{browser.a(:class => "click me").click}},
+    html:   html_class,
+    regexp: regexp(:class),
+    string: string(:class)},
 
   css: {
-    html:   %{<a href="clicked.htm" id="click-me">click me</a>},
+    html:   html_id,
     string: %{browser.a(:css => "[id=click-me]").click}},
+
+  element_by_xpath:{
+    html:   html_id,
+    string: %{browser.element_by_xpath("//a[@id='click-me']").click}},
 
   for: false,
 
   href: {
-    html:   simple_html_example,
-    regexp: %{browser.a(:href => /clicked/).click},
+    html:   html,
+    regexp: regexp(:href),
     string: %{browser.a(:href => "clicked.htm").click}},
 
   html: {
-    html:   %{<a href="clicked.htm" onclick="new Ajax.Request('007')">click me</a>\n<a href="clicked.htm" onclick="new Ajax.Request('42')">click me</a>},
+    html:   %{<a href="clicked.htm" onclick="new Ajax.Request('007')">click me</a><br><a href="clicked.htm" onclick="new Ajax.Request('42')">click me</a>},
     regexp: %{browser.a(:html => /007/).click},
     string: %{TODO}},
 
   id: {
-    html:   %{<a href="clicked.htm" id="click-me">click me</a>},
-    regexp: %{browser.a(:id => /click/).click},
-    string: %{browser.a(:id => "click-me").click}},
+    html:   html_id,
+    regexp: regexp(:id),
+    string: string(:id)},
 
   index: {
-    html:     simple_html_example,
-    implicit: %{browser.a.click},
+    html:     html,
+    none: %{browser.a.click},
     integer:  %{browser.a(:index => 0).click}},
 
   method: false,
 
   name: {
-    html:   %{<a href="clicked.htm" name="click-me">click me</a>},
-    regexp: %{browser.a(:name => /click/).click},
-    string: %{browser.a(:name => "click-me").click}},
+    html:   html_name,
+    regexp: regexp(:name),
+    string: string(:name)},
 
   src: false,
 
   text: {
-    html:   simple_html_example,
-    regexp: %{browser.a(:text => /click/).click},
-    string: %{browser.a(:text => "click me")}},
+    html:   html,
+    regexp: regexp(:text),
+    string: string(:text)},
 
   title: {
-    html:   %{<a href="clicked.htm" title="click me">click me</a>},
-    regexp: %{browser.a(:title => /click/)},
-    string: %{browser.a(:title => "click me")}},
+    html:   html_title,
+    regexp: regexp(:title),
+    string: string(:title)},
 
   url: {
-    html:   simple_html_example,
-    regexp: %{browser.a(:url => /clicked/).click},
+    html:   html,
+    regexp: regexp(:url),
     string: %{browser.a(:url => "clicked.htm").click}},
 
   value: false,
 
   xpath:{
-    element_by_xpath: %{browser.element_by_xpath("//a[@id='click-me']").click},
-    html:             %{<a href="clicked.htm" id="click-me">click me</a>},
-    xpath:            %{browser.a(:xpath => "//a[@id='click-me']").click}},
+    html:   html_id,
+    string: %{browser.a(:xpath => "//a[@id='click-me']").click}},
 
   multiple_attributes: {
     hash: %{browser.a(:text => "click me", :index => 1).click},
-    html: %{#{simple_html_example}\n#{simple_html_example}}},
+    html: html_double},
 
   collection: {
-    example: %{browser.as.each {|a| puts a.text}},
-    html:    %{#{simple_html_example}\n#{simple_html_example}}}}
+    none: %{browser.as.each {|a| puts a.text}},
+    html:    html_double}
+  }
+end
+def html(attribute = nil)
+  html = %{<a href="clicked.htm"#{attribute}>click me</a>}
+end
+
+if __FILE__ == $0
+  create_html(body)
+end
