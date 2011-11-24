6a00141b72701735fe6d017eb02e1338565069d1
Zeljko Filipin <zeljko.filipin@gmail.com>
2011-11-24 22:08:29 +0100

    nicer element table

--- rb/create_element_table.rb
+++ rb/create_element_table.rb
@@ -21,7 +21,7 @@ def string(how)
 end
 
 def create_html(body)
-  header = "<html><head><title>a element</title></head><body><h1>a element</h1><table border=\"1\"><tbody><tr><th>how</th><th>html</th><th>regexp</th><th>string</th><th>other</th>"
+  header = "<html><head><title>a element</title></head><body><h1>a element</h1><table border=\"1\"><tbody><tr><th>how</th><th>html</th><th>examples</th>"
   footer = "</tbody></table></body></html>"
   table = header + body + footer
   File.open("reference/a.htm", "w") {|file| file.write(table)}
@@ -31,26 +31,20 @@ def body
   element.each do |how, data|
     if data
 
+      examples = ""
       html = ""
-      regexp = ""
-      string = ""
-      other = ""
       data.each do |key, value|
         if key == :html
           require "cgi"
           html = CGI::escapeHTML(value).gsub("&lt;br&gt;", "<br>")
-        elsif key == :regexp
-          regexp = value
-        elsif key == :string
-          string = value
         else
-          other << "#{key}: #{value}<br>"
+          examples << "#{value} <i># #{key}</i><br>"
         end
       end
 
-      table << "<tr><td>#{how}</td><td>#{html}</td><td>#{regexp}</td><td>#{string}</td><td>#{other}</td></tr>"
+      table << "<tr><td>#{how}</td><td>#{html}</td><td>#{examples}</td></tr>"
     else
-      table << "<tr><td>#{how}</td><td colspan='4'>not supported</td></tr>"
+      table << "<tr><td>#{how}</td><td colspan='2'>not supported</td></tr>"
     end
   end
   table
