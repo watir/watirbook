c91c9d3cb9a3d8d5ea8a6e3562b0e66d19186d8d
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-12-28 23:40:08 +0100

    deleted unused ruby files

--- lib/inspect/safariwatir-elements.rb
+++
@@ -1,6 +0,0 @@
-# get a list of all HTML elements supported by safariwatir gem
-
-require "safariwatir"
-require "./subclasses_of"
-
-puts subclasses_of(Watir::Container::HtmlElement, "Watir::Container")
--- lib/inspect/watir-elements.rb
+++
@@ -1,8 +0,0 @@
-# get a list of all HTML elements and collections supported by watir gem
-
-require "watir/ie"
-require "./subclasses_of"
-
-puts subclasses_of(Watir::Element, "Watir")
-puts
-puts subclasses_of(Watir::ElementCollections, "Watir")
