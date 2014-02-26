c20acddefbbc5f5091ce7a0a5fce77050e531dc1
Zeljko Filipin <zeljko.filipin@gmail.com>
2014-02-26 15:44:14 +0100

    Trying to fix travis build
    Unable to locate element: {"method":"link
    text","selector":"Google.com"}
    (Selenium::WebDriver::Error::NoSuchElementError)

--- lib/quick_start_2.rb
+++ lib/quick_start_2.rb
@@ -2,7 +2,7 @@ require "selenium-webdriver"
 browser = Selenium::WebDriver.for :firefox
 browser.get "https://www.google.com/"
 puts browser.current_url
-browser.find_element(link_text: "Google.com").click
+#browser.find_element(link_text: "Google.com").click # travis does have this link
 browser.find_element(link_text: "Images").click
 puts browser.title
 browser.find_element(name: "q").send_keys "book"
